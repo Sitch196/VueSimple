379685239c7857fe42305c29fdf8a3e0e24fbe78
Sitch196 <sichinava1996@gmail.com>
2024-01-11 00:55:08 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue';
+</script>
+
+<template>
+  <TodoHeader />
+
+  <RouterView />
+</template>
+
+
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Rubik', sans-serif;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <h2>todo Item</h2>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
